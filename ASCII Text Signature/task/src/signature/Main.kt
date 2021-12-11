e8aa074047924d4b07cfe52f4f91ac8610805516
Anatoliy <anartius@gmail.com>
2021-12-11 19:22:30 +0100

    Finish of the stage 3.

--- ASCII Text Signature/task/src/signature/Main.kt
+++ ASCII Text Signature/task/src/signature/Main.kt
@@ -1,9 +1,80 @@
 package signature
 
+import kotlin.math.ceil
+import kotlin.math.max
+
 fun main() {
+    print("Enter name and surname: ")
     val name = readLine()!!
-    val nameLength = name.length
-    println("*".repeat(nameLength + 4))
-    println("* $name *")
-    println("*".repeat(nameLength + 4))
+
+    print("Enter person's status: ")
+    val status = readLine()!!
+
+    var letterTop = ""
+    var letterMiddle = ""
+    var letterBottom = ""
+    name.forEach { letterTop += " " + getPartOfLetter(it, 0) }
+    name.forEach { letterMiddle += " " + getPartOfLetter(it, 1) }
+    name.forEach { letterBottom += " " + getPartOfLetter(it, 2) }
+
+    val length = max(letterTop.length - 1, status.length)
+
+    val nameLeftSpace = ceil(((length - letterTop.length).toDouble() / 2)).toInt() + 1
+    val nameRightSpace = if ((length - (letterTop.length - 1)) % 2 == 0) {
+        ceil(((length - letterTop.length).toDouble() / 2)).toInt() + 2
+    } else (length - letterTop.length) / 2 + 3
+
+    val statusLeftSpace = (length - status.length) / 2 + 2
+    val statusRightSpace = if ((length - status.length) % 2 == 0) {
+        (length - status.length) / 2 + 2
+    } else (length - status.length) / 2 + 3
+
+    println("*".repeat(length + 6))
+
+    println("*" + " ".repeat(nameLeftSpace) + letterTop +
+            " ".repeat(nameRightSpace) + "*")
+    println("*" + " ".repeat(nameLeftSpace) + letterMiddle +
+            " ".repeat(nameRightSpace) + "*")
+    println("*" + " ".repeat(nameLeftSpace) + letterBottom +
+            " ".repeat(nameRightSpace) + "*")
+
+    println("*" + " ".repeat(statusLeftSpace) + status +
+            " ".repeat(statusRightSpace) + "*")
+
+    println("*".repeat(length + 6))
+
+}
+
+fun getPartOfLetter (letter: Char, level: Int) : String {
+
+    val partOfLetter = when (letter.lowercase().first()) {
+        'a' -> mutableListOf("____", "|__|", "|  |")
+        'b' -> mutableListOf("___ ", "|__]", "|__]")
+        'c' -> mutableListOf("____", "|   ", "|___")
+        'd' -> mutableListOf("___ ", "|  \\", "|__/")
+        'e' -> mutableListOf("____", "|___", "|___")
+        'f' -> mutableListOf("____", "|___", "|   ")
+        'g' -> mutableListOf("____", "| __", "|__]")
+        'h' -> mutableListOf("_  _", "|__|", "|  |")
+        'i' -> mutableListOf("_", "|", "|")
+        'j' -> mutableListOf(" _", " |", "_|")
+        'k' -> mutableListOf("_  _", "|_/ ", "| \\_")
+        'l' -> mutableListOf("_   ", "|   ", "|___")
+        'm' -> mutableListOf("_  _", "|\\/|", "|  |")
+        'n' -> mutableListOf("_  _", "|\\ |", "| \\|")
+        'o' -> mutableListOf("____", "|  |", "|__|")
+        'p' -> mutableListOf("___ ", "|__]", "|   ")
+        'q' -> mutableListOf("____", "|  |", "|_\\|")
+        'r' -> mutableListOf("____", "|__/", "|  \\")
+        's' -> mutableListOf("____", "[__ ", "___]")
+        't' -> mutableListOf("___", " | ", " | ")
+        'u' -> mutableListOf("_  _", "|  |", "|__|")
+        'v' -> mutableListOf("_  _", "|  |", " \\/ ")
+        'w' -> mutableListOf("_ _ _", "| | |", "|_|_|")
+        'x' -> mutableListOf("_  _", " \\/ ", "_/\\_")
+        'y' -> mutableListOf("_   _", " \\_/ ", "  |  ")
+        'z'-> mutableListOf("___ ", "  / ", " /__")
+        else -> mutableListOf("    ", "    ", "    ")
+    }
+    return partOfLetter[level]
 }
